731812b63cd2155b0bd91cecec1ebc09e64f2525
mazaevroman33@mail.ru <r.mazaev@univ.priv>
2021-09-16 12:09:13 +0300

    Add second /n

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class Hello{
 	public static void main(String[] args) {
 		System.out.println("Hello World!1");
+		System.out.println("\n");
                 System.out.println("New Feature!");
 		System.out.println("\n");
 	}
